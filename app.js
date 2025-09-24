f1b7abe07719a93ac53ea61f25e6ed31f5270fb0
Rodogaby1985 <rodogaby85@gmail.com>
2025-09-24 15:15:32 -0300

    Update app.js

--- app.js
+++ app.js
@@ -58,6 +58,13 @@ app.use((req, _res, next) => {
 // [EDITADO SUCURSAL] - Rutas bajo /suc
 app.use('/suc', authRoutes);
 app.use('/suc/api', shippingRoutes);
+app.get('/suc', (req, res) => {
+  res.send(`
+    <h1>Aplicación de Envíos Sucursal</h1>
+    <p>Para instalar la aplicación en tu Tienda Nube, hacé clic en el siguiente enlace:</p>
+    <a href="/suc/install">Instalar Aplicación</a>
+  `);
+});
 
 // Ruta de Health Check
 app.get('/health', (_req, res) => {
